caf7aec33db3e47f14fb54eaed0fe1c839375589
Courtney Phillips <courtney.mae.phillips@gmail.com>
2016-01-08 10:09:30 -0800

    Add logic for myStore method; test pass.

--- lib/myhash.rb
+++ lib/myhash.rb
@@ -1,11 +1,23 @@
+require('pry')
+
 class MyHash
-
   define_method(:initialize) do
+    @hash = []
+  end
 
+  define_method(:myStore) do |key, value|
+    @hash.push([key, value])
   end
 
   define_method(:myFetch) do |key|
-
+    returned_value = nil
+    @hash.each do |entry|
+      if entry.include?(key)
+        returned_value = entry[1]
+      else
+        returned_value = "not available"
+      end
+    end
+    returned_value
   end
-
 end
