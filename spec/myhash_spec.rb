9bd4fb487fb7220c8997af8a3ed0eaf2ddbe511f
Courtney Phillips <courtney.mae.phillips@gmail.com>
2016-01-08 09:26:18 -0800

    Add test for retrieving value using key.

---
+++ spec/myhash_spec.rb
@@ -0,0 +1,9 @@
+describe(MyHash) do
+  describe("#myFetch") do
+    it("retrieves a stored value by its key") do
+      test_hash = MyHash.new()
+      test_hash.myStore("kitten", "cute")
+      expect(test_hash.myFetch("kitten")).to(eq("cute"))
+    end
+  end
+end
