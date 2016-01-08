574775fe1483185fe7a89478d0a27ea16c39b58c
Courtney Phillips <courtney.mae.phillips@gmail.com>
2016-01-08 10:13:36 -0800

    Add additional test for both empty MyHash objects, and returning relevant lengths of MyHash objects.

--- spec/myhash_spec.rb
+++ spec/myhash_spec.rb
@@ -8,6 +8,13 @@ describe(MyHash) do
       expect(test_hash.length).to(eq(0))
     end
   end
+  describe("#length") do
+    it("returns the length of a MyHash object") do
+      test_hash = MyHash.new()
+        test_hash.myStore("kitten", "cute")
+      expect(test_hash.length).to(eq(1))
+    end
+  end
   describe("#myStore") do
     it("stores a key and corresponding value") do
       test_hash = MyHash.new()
