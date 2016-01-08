0fd0e2ce8afef83a33dce9dabed8c53e54a03130
Courtney Phillips <courtney.mae.phillips@gmail.com>
2016-01-08 10:09:18 -0800

    Add test for myStore method.

--- spec/myhash_spec.rb
+++ spec/myhash_spec.rb
@@ -2,6 +2,13 @@ require('rspec')
 require('./lib/myhash.rb')
 
 describe(MyHash) do
+  describe("#myStore") do
+    it("stores a key and corresponding value") do
+      test_hash = MyHash.new()
+      test_hash.myStore("kitten", "cute")
+      expect(test_hash.length).to(eq(["kitten", "cute"]))
+    end
+  end
   describe("#myFetch") do
     it("retrieves a stored value by its key") do
       test_hash = MyHash.new()
